37435f6a7939a3434bd48779639cc98a21188940
rainbow007-hub <970119238@qq.com>
2021-06-22 19:19:00 +0800

    用户、动态整合

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,12 +2,10 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -18,8 +16,8 @@ public class Application extends SpringBootServletInitializer {
         return new String();
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Application.class);
+//    }
 }
